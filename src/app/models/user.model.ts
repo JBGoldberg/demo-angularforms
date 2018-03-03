f8abf9aa5e884f0e4861fb01fd5cb849032a4a20
Jim Bruno Goldberg <jimbrunogoldberg@gmail.com>
2018-03-03 16:31:12 -0300

    Added user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,14 @@
+
+export class User {
+
+  public first_name: string;
+  public last_name: string;
+  public username: string;
+  public email: string;
+  public password: string;
+  public id: number;
+
+  constructor() {
+  }
+
+}
